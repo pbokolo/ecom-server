fe0ab06c4cce09f0d0126d00a772a9bdc95aa9b3
pbokolo <dev.pbokolo@gmail.com>
2024-01-10 09:58:35 +0200

    Creating local ressources to be returned

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require("express");
-
 const app = express();
+const stuffs = [
+  {
+    title: "Samsung Galaxy S10",
+    description: "Smartphone de seconde main en bon état",
+    prix: 150,
+    monnaie: { code: "usd", label: "$" },
+  },
+  {
+    title: "Cable USB-C",
+    description:
+      "Cable usb-c pour la recharge des appareils et le transfert des données",
+    prix: 4500,
+    monnaie: { code: "cdf", label: "FC" },
+  },
+];
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Bien reçu" });
+  res.status(200).json({ stuffs });
 });
 
 module.exports = app;
