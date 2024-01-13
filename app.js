233bedcb6cd0f40bc0b24cfb63f216ef1158bf9f
pbokolo <dev.pbokolo@gmail.com>
2024-01-13 07:28:45 +0200

    Successfully connecting to the database

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+
+// Trying to connect with the database
+mongoose
+  .connect(
+    "mongodb+srv://pierrot:pbtomongopossible12@exos.uijotis.mongodb.net/ecom?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch((error) => console.log("Connexion à MongoDB échouée !", error));
+
 const stuffs = [
   {
     title: "Samsung Galaxy S10",
