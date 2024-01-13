667a6036952a4681553e66035f84a3769ed38ea0
pbokolo <dev.pbokolo@gmail.com>
2024-01-13 08:13:20 +0200

    Handling the post route

--- app.js
+++ app.js
@@ -12,12 +12,26 @@ mongoose
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch((error) => console.log("Connexion à MongoDB échouée !", error));
 
+app.use(express.json());
+
 app.get("/products/all", async (req, res) => {
   try {
     const products = await Product.find();
     res.status(200).json({ products });
   } catch (error) {
-    res.status(500).json({ message: "Erreur interne", error });
+    res.status(500).json({ message: "Internal error", error });
+  }
+});
+
+app.post("/products/product", async (req, res) => {
+  const product = new Product({ ...req.body });
+  // const p = { ...req.body };
+  // res.status(201).json({ p });
+  try {
+    await product.save();
+    res.status(201).json({ message: "Product saved" });
+  } catch (error) {
+    res.status(501).json({ message: "Internal error", error });
   }
 });
 
--- model/Product.js
+++ model/Product.js
@@ -4,7 +4,7 @@ const productSchema = mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
-  currency: { type: String, required: true },
+  currency: { type: Object, required: true },
 });
 
 // Exports the model to make it usable
